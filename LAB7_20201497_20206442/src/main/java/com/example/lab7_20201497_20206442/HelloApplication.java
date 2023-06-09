d06b708722aa7955dbe6ad825e861ebb190a6975
Milene <a20206456@pucp.edu.pe>
2023-06-08 20:24:27 -0500

    primer commit

---
+++ LAB7_20201497_20206442/src/main/java/com/example/lab7_20201497_20206442/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.lab7_20201497_20206442;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
